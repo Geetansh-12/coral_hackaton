1fd72db302a4084e655040ca80a788f87bf5b256
Geetansh vikram <chitranshvikram12@gmail.com>
2026-05-27 03:42:44 +0530

    chore: remove accidentally committed temp files

--- scripts/test-coral.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  const result = execSync('coral.exe sql --format json "SELECT * FROM \'test.csv\'"');
-  console.log(result.toString());
-} catch (e) {
-  console.log('Error:', e.message);
-  console.log('Stderr:', e.stderr?.toString());
-}
